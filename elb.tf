4856aada44be33cd48d724eb6a14aa4546308147
Callum <callumccr@gmail.com>
2020-10-01 18:18:41 +0100

    Gitlab README commit

--- elb.tf
+++ elb.tf
@@ -125,7 +125,7 @@ resource "aws_lb_target_group_attachment" "default" {
   port             = var.target_group_protocol
 
   depends_on = [
-    data.aws_instances.emr_master_instances
+    data.aws_instances.emr_master_instances,
     aws_emr_cluster.default
   ]
 }
