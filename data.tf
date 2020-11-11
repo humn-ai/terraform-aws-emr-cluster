2a614e6abb496eada947cd545191260e538ec806
Callum <callumccr@gmail.com>
2020-11-11 15:54:24 +0000

    removed unneccessary data sources

--- data.tf
+++ data.tf
@@ -12,18 +12,3 @@ data "aws_instances" "emr_master_instances" {
     aws_emr_cluster.default
   ]
 }
-
-# data "aws_instances" "emr_core_instances" {
-#   instance_tags = {
-#     Name = "dev-humnai-kylin"
-#   }
-
-#   filter {
-#     name   = "tag:aws:elasticmapreduce:instance-group-role"
-#     values = ["SLAVE"]
-#   }
-
-#   depends_on = [
-#     aws_emr_cluster.default
-#   ]
-# }
